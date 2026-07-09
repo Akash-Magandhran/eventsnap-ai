db498058298975661607ff7aacaa8ce33a0ddd78
Akash-Magandhran <strakash456@gmail.com>
2026-07-09 23:25:10 +0530

    Frontend production ready

--- backend/eventsnap/settings.py
+++ backend/eventsnap/settings.py
@@ -90,15 +90,11 @@ ASGI_APPLICATION = 'eventsnap.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='eventsnaphost_db'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
